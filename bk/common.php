894d1da6137c5ec58482268139197045faa48616
huangxiujie <huangxiujie@163.com>
2016-12-31 17:27:12 +0800

    clear old code of goods and order

--- bk/common.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'leancloud-sdk/src/autoload.php';
-use \LeanCloud\Client;
-use \LeanCloud\Object;
-use \LeanCloud\Query;
-// 参数依次为 AppId, AppKey, MasterKey
-Client::initialize("SgHcsYqoLaFTG0XDMD3Gtm0I-gzGzoHsz", "xdv2nwjUK5waNglFoFXkQcxP", "v3P5xzDa0b5CStO0xX0biHpT");
