dff47803ec3bf30063e6568706fa32abbc74a8e2
huangxiujie <huangxiujie@163.com>
2016-12-31 22:55:14 +0800

    base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '/lendoo-web';
+$baseUrl = str_replace('\\','/',dirname($_SERVER['SCRIPT_NAME']));
+$baseUrl = trim($baseUrl,'/');
+$config['base_url'] = empty($baseUrl) ? '/' : "/$baseUrl/";
 
 /*
 |--------------------------------------------------------------------------
