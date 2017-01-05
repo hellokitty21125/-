b18f8d3ebcc65d732d3afec4dd3c43e7cd434381
iMac <huangxiujie@163.com>
2017-01-05 11:06:02 +0800

    nonce_str

--- application/controllers/WXPay.php
+++ application/controllers/WXPay.php
@@ -35,7 +35,7 @@ class WXPay extends BaseController {
 		$jsapi->SetAppid($UnifiedOrderResult["appid"]);
 		$timeStamp = time();
 		$jsapi->SetTimeStamp("$timeStamp");
-		$jsapi->SetNonceStr(WxPayApi::getNonceStr());
+		$jsapi->SetNonceStr($UnifiedOrderResult['nonce_str']);
 		$jsapi->SetPackage("prepay_id=" . $UnifiedOrderResult['prepay_id']);
 		$jsapi->SetSignType("MD5");
 		$jsapi->SetPaySign($jsapi->MakeSign());
