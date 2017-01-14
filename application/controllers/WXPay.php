0242ab0da2645df66a8008c004cbf0c5ad0915a3
huangxiujie <huangxiujie@163.com>
2017-01-14 22:27:40 +0800

    get openid at server

--- application/controllers/WXPay.php
+++ application/controllers/WXPay.php
@@ -4,26 +4,26 @@ require_once __DIR__ . '/../third_party/wxpay/WxPay.Api.php';
 
 class WXPay extends BaseController {
 	function index() {
-// 		初始化值对象
+		// 		初始化值对象
 		$input = new WxPayUnifiedOrder();
-// 		文档提及的参数规范：商家名称-销售商品类目
+		// 		文档提及的参数规范：商家名称-销售商品类目
 		$input->SetBody("灵动商城-手机");
-// 		订单号应该是由小程序端传给服务端的，在用户下单时即生成，demo中取值是一个生成的时间戳
+		// 		订单号应该是由小程序端传给服务端的，在用户下单时即生成，demo中取值是一个生成的时间戳
 		$input->SetOut_trade_no(time().'');
-// 		费用应该是由小程序端传给服务端的，在用户下单时告知服务端应付金额，demo中取值是1，即1分钱
+		// 		费用应该是由小程序端传给服务端的，在用户下单时告知服务端应付金额，demo中取值是1，即1分钱
 		$input->SetTotal_fee("1");
 		$input->SetNotify_url("http://paysdk.weixin.qq.com/example/notify.php");
 		$input->SetTrade_type("JSAPI");
-// 		由小程序端传给服务端
-		$input->SetOpenid($this->input->post('openId'));
-// 		向微信统一下单，并返回order，它是一个array数组
+		// 		由小程序端传给服务端
+		$input->SetOpenid($this->getSession()->openid);
+		// 		向微信统一下单，并返回order，它是一个array数组
 		$order = WxPayApi::unifiedOrder($input);
-// 		json化返回给小程序端
+		// 		json化返回给小程序端
 		header("Content-Type: application/json");
-                echo $this->getJsApiParameters($order);
+		echo $this->getJsApiParameters($order);
 	}
-        
-        public function getJsApiParameters($UnifiedOrderResult)
+
+	private function getJsApiParameters($UnifiedOrderResult)
 	{
 		if(!array_key_exists("appid", $UnifiedOrderResult)
 		|| !array_key_exists("prepay_id", $UnifiedOrderResult)
@@ -41,5 +41,12 @@ class WXPay extends BaseController {
 		$jsapi->SetPaySign($jsapi->MakeSign());
 		$parameters = json_encode($jsapi->GetValues());
 		return $parameters;
+	}
+	
+	private function getSession() {
+		$code = $this->input->post('code');
+		$url = 'https://api.weixin.qq.com/sns/jscode2session?appid='.WxPayConfig::APPID.'&secret='.WxPayConfig::APPSECRET.'&js_code='.$code.'&grant_type=authorization_code';
+		$response = json_decode(file_get_contents($url));
+		return $response;
 	}
 }
