15db9a22f8ca6f12b86b8d47047e258be92ab7c6
huangxiujie <huangxiujie@126.com>
2017-03-21 10:16:50 +0800

    获取小程序二维码

--- application/controllers/WXPay.php
+++ application/controllers/WXPay.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/BaseController.php';
 require_once __DIR__ . '/../third_party/wxpay/WxPay.Api.php';
 
 class WXPay extends BaseController {
+	// pay order
 	function index() {
 		// 		初始化值对象
 		$input = new WxPayUnifiedOrder();
@@ -43,10 +44,51 @@ class WXPay extends BaseController {
 		return $parameters;
 	}
 	
+	// get openid & session_key
 	public function getSession() {
 		$code = $this->input->post('code');
 		$url = 'https://api.weixin.qq.com/sns/jscode2session?appid='.WxPayConfig::APPID.'&secret='.WxPayConfig::APPSECRET.'&js_code='.$code.'&grant_type=authorization_code';
-                header("Content-Type: application/json");
-                echo file_get_contents($url);
+            header("Content-Type: application/json");
+            echo file_get_contents($url);
+	}
+
+	// get access token
+	private function getAccessToken() {
+		$url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.WxPayConfig::APPID.'&secret='.WxPayConfig::APPSECRET;
+            // header("Content-Type: application/json");
+            return file_get_contents($url);
+	}
+
+	// 服务端生成图片
+	public function getQRCode() {
+		// 获取access_token
+		$accessTokenObject = json_decode(file_get_contents('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.WxPayConfig::APPID.'&secret='.WxPayConfig::APPSECRET));
+		// 拼接微信服务端获取二维码需要的url，见文档https://mp.weixin.qq.com/debug/wxadoc/dev/api/qrcode.html
+		$url = 'https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode?access_token=' . $accessTokenObject->access_token;
+		$json = '{"path": "pages/index/index", "width": 430}';
+		$ch = curl_init();
+		//设置超时
+		curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+		//如果有配置代理这里就设置代理
+		if(WxPayConfig::CURL_PROXY_HOST != "0.0.0.0" 
+			&& WxPayConfig::CURL_PROXY_PORT != 0){
+			curl_setopt($ch,CURLOPT_PROXY, WxPayConfig::CURL_PROXY_HOST);
+			curl_setopt($ch,CURLOPT_PROXYPORT, WxPayConfig::CURL_PROXY_PORT);
+		}
+		curl_setopt($ch,CURLOPT_URL, $url);
+		curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,TRUE);
+		curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);//严格校验
+		//设置header
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		//要求结果为字符串且输出到屏幕上
+		header('Content-Type: image/jpeg');
+		//post提交方式
+		curl_setopt($ch, CURLOPT_POST, TRUE);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+		//运行curl
+		$data = curl_exec($ch);
+		//返回结果
+		curl_close($ch);
+		echo $data;
 	}
 }
