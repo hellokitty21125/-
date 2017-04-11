15073d61ae7a9862abbace1d7a06de0d7bc3e404
黄秀杰 <huangxiujie@126.com>
2017-04-11 16:11:53 +0800

    分页优化

--- application/controllers/Goods.php
+++ application/controllers/Goods.php
@@ -44,53 +44,20 @@ class Goods extends BaseController {
 	// 商品列表-adminlte
 	public function index() {
 		// 获取get参数
-		$pageIndex = $this->input->get('per_page', 1);
+		$pageIndex = $this->input->get('per_page');
 		// 分页查询数据
 		$query = new Query("Goods");
 		$query->_include("category");
 		$query->ascend("updatedAt");
-		$query->limit($this->pageSize);
-		$query->skip($this->pageSize * ($pageIndex - 1));
+		$query->limit($this->config->item('per_page'));
+		$query->skip($this->config->item('per_page') * ($pageIndex - 1));
 		$result = $query->find();
 		// 分页控件
 		$this->load->library('pagination');
-		// 路径前缀
-		$config['base_url'] = base_url('index.php/goods/index');
+		// url路径前缀
+		$config['base_url'] = base_url(uri_string());
 		// 总条数
 		$config['total_rows'] = (new Query("Goods"))->count();
-		// 左右侧显示页数
-		$config['num_links'] = 5;
-		// 每页条数
-		$config['per_page'] = $this->pageSize; 
-		// 页码模式
-		$config['use_page_numbers'] = true; 
-		// enable_query_strings风格
-		$config['page_query_string'] = true;
-		// 整个分页控件
-		$config['full_tag_open'] = '<ul class="pagination">';
-		$config['full_tag_close'] = '</ul>';
-		// 第一页
-		$config['first_link'] = '&lt;&lt;';
-		$config['first_tag_open'] = '<li>';
-		$config['first_tag_close'] = '</li>';
-		// 最后一页
-		$config['last_link'] = '&gt;&gt;';
-		$config['last_tag_open'] = '<li>';
-		$config['last_tag_close'] = '</li>';
-		// 上一页
-		$config['prev_link'] = '&lt;';
-		$config['prev_tag_open'] = '<li>';
-		$config['prev_tag_close'] = '</li>';
-		// 下一页
-		$config['next_link'] = '&gt;';
-		$config['next_tag_open'] = '<li>';
-		$config['next_tag_close'] = '</li>';
-		// 页码
-		$config['num_tag_open'] = '<li>';
-		$config['num_tag_close'] = '</li>';
-		// 当前页
-		$config['cur_tag_open'] = '<li class="active"><a href="javascript:;">';
-		$config['cur_tag_close'] = '</li></a>';
 		// 初始化
 		$this->pagination->initialize($config); 
 		$data['pagination'] = $this->pagination->create_links();
