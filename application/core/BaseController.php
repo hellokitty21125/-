dfa5507bd4091e403fb06bf37b6c62823788a2f7
黄秀杰 <huangxiujie@126.com>
2017-04-11 21:26:55 +0800

    composer auto load

--- application/core/BaseController.php
+++ application/core/BaseController.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-require_once __DIR__ . '/../../vendor/autoload.php';
+// require_once __DIR__ . '/../../vendor/autoload.php';
 
 use \LeanCloud\Client;
 
