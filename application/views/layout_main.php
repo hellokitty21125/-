1907d42967b709e70b3172a13f77fca01f190c9d
黄秀杰 <huangxiujie@126.com>
2017-04-12 13:46:10 +0800

    再次清理js库

--- application/views/layout_main.php
+++ application/views/layout_main.php
@@ -39,16 +39,39 @@
   <script src="https://cdn.bootcss.com/jquery/2.2.3/jquery.min.js"></script>
   <!-- Bootstrap 3.3.6 -->
   <script src="https://cdn.bootcss.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  <script src="https://cdn.bootcss.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <link href="https://cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
   <!-- jQuery UI 1.11.4 -->
-  <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+  <script src="/assets/node_modules/admin-lte/plugins/jQueryUI/jquery-ui.min.js"></script>
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
+
+  <!-- Morris.js charts -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+  <script src="/assets/node_modules/admin-lte/plugins/morris/morris.min.js"></script>
+  <!-- Sparkline -->
+  <script src="/assets/node_modules/admin-lte/plugins/sparkline/jquery.sparkline.min.js"></script>
+  <!-- jvectormap -->
+  <script src="/assets/node_modules/admin-lte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+  <script src="/assets/node_modules/admin-lte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+  <!-- jQuery Knob Chart -->
+  <script src="/assets/node_modules/admin-lte/plugins/knob/jquery.knob.js"></script>
+  <!-- daterangepicker -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+  <script src="/assets/node_modules/admin-lte/plugins/daterangepicker/daterangepicker.js"></script>
+  <!-- datepicker -->
+  <script src="/assets/node_modules/admin-lte/plugins/datepicker/bootstrap-datepicker.js"></script>
+  <!-- Bootstrap WYSIHTML5 -->
+  <script src="/assets/node_modules/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+  <!-- Slimscroll -->
+  <script src="/assets/node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+  <!-- FastClick -->
+  <script src="/assets/node_modules/admin-lte/plugins/fastclick/fastclick.js"></script>
+  <!-- AdminLTE App -->
+  <script src="/assets/node_modules/admin-lte/dist/js/app.min.js"></script>
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
   <div class="wrapper">
@@ -572,30 +595,5 @@
 <script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
-<!-- Bootstrap 3.3.6 -->
-<script src="/assets/node_modules/admin-lte/bootstrap/js/bootstrap.min.js"></script>
-<!-- Morris.js charts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="/assets/node_modules/admin-lte/plugins/morris/morris.min.js"></script>
-<!-- Sparkline -->
-<script src="/assets/node_modules/admin-lte/plugins/sparkline/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
-<script src="/assets/node_modules/admin-lte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="/assets/node_modules/admin-lte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="/assets/node_modules/admin-lte/plugins/knob/jquery.knob.js"></script>
-<!-- daterangepicker -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-<script src="/assets/node_modules/admin-lte/plugins/daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
-<script src="/assets/node_modules/admin-lte/plugins/datepicker/bootstrap-datepicker.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="/assets/node_modules/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
-<script src="/assets/node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="/assets/node_modules/admin-lte/plugins/fastclick/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="/assets/node_modules/admin-lte/dist/js/app.min.js"></script>
 </body>
 </html>
