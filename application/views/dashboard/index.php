a099a27a2b6d4ae24fe9bc35d3e8ae1867c7f50a
黄秀杰 <huangxiujie@126.com>
2017-04-15 00:35:01 +0800

    将js/css从layout_main转到dashboard/index.php

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,3 +1,35 @@
+  <!-- iCheck -->
+  <!-- <link rel="stylesheet" href="/bower_components/AdminLTE/plugins/iCheck/flat/blue.css"> -->
+  <!-- Morris chart -->
+  <link rel="stylesheet" href="/bower_components/AdminLTE/plugins/morris/morris.css">
+  <!-- jQuery UI 1.11.4 -->
+  <script src="/bower_components/AdminLTE/plugins/jQueryUI/jquery-ui.min.js"></script>
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Morris.js charts -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+  <script src="/bower_components/AdminLTE/plugins/morris/morris.min.js"></script>
+  <!-- Sparkline -->
+  <script src="/bower_components/AdminLTE/plugins/sparkline/jquery.sparkline.min.js"></script>
+  <!-- jvectormap -->
+  <script src="/bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+  <script src="/bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+  <!-- jQuery Knob Chart -->
+  <script src="/bower_components/AdminLTE/plugins/knob/jquery.knob.js"></script>
+  <!-- daterangepicker -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+  <script src="/bower_components/AdminLTE/plugins/daterangepicker/daterangepicker.js"></script>
+  <!-- datepicker -->
+  <script src="/bower_components/AdminLTE/plugins/datepicker/bootstrap-datepicker.js"></script>
+  <!-- Bootstrap WYSIHTML5 -->
+  <script src="/bower_components/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+  <!-- Slimscroll -->
+  <script src="/bower_components/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js"></script>
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
         <section class="content-header">
--- application/views/layout_main.php
+++ application/views/layout_main.php
@@ -22,54 +22,11 @@
   <!-- AdminLTE Skins. Choose a skin from the css/skins
   folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="/bower_components/AdminLTE/dist/css/skins/_all-skins.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="/bower_components/AdminLTE/plugins/iCheck/flat/blue.css">
-  <!-- Morris chart -->
-  <link rel="stylesheet" href="/bower_components/AdminLTE/plugins/morris/morris.css">
-  <!-- jvectormap -->
-  <link rel="stylesheet" href="/bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
-  <!-- Date Picker -->
-  <link rel="stylesheet" href="/bower_components/AdminLTE/plugins/datepicker/datepicker3.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="/bower_components/AdminLTE/plugins/daterangepicker/daterangepicker.css">
-  <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="/bower_components/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
-
   <!-- jQuery 2.2.3 -->
   <script src="https://cdn.bootcss.com/jquery/2.2.3/jquery.min.js"></script>
   <!-- Bootstrap 3.3.6 -->
   <script src="https://cdn.bootcss.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <link href="https://cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-  <!-- jQuery UI 1.11.4 -->
-  <script src="/bower_components/AdminLTE/plugins/jQueryUI/jquery-ui.min.js"></script>
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-  <!-- Morris.js charts -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-  <script src="/bower_components/AdminLTE/plugins/morris/morris.min.js"></script>
-  <!-- Sparkline -->
-  <script src="/bower_components/AdminLTE/plugins/sparkline/jquery.sparkline.min.js"></script>
-  <!-- jvectormap -->
-  <script src="/bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-  <script src="/bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-  <!-- jQuery Knob Chart -->
-  <script src="/bower_components/AdminLTE/plugins/knob/jquery.knob.js"></script>
-  <!-- daterangepicker -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-  <script src="/bower_components/AdminLTE/plugins/daterangepicker/daterangepicker.js"></script>
-  <!-- datepicker -->
-  <script src="/bower_components/AdminLTE/plugins/datepicker/bootstrap-datepicker.js"></script>
-  <!-- Bootstrap WYSIHTML5 -->
-  <script src="/bower_components/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-  <!-- Slimscroll -->
-  <script src="/bower_components/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js"></script>
-  <!-- FastClick -->
-  <script src="/bower_components/AdminLTE/plugins/fastclick/fastclick.js"></script>
   <!-- AdminLTE App -->
   <script src="/bower_components/AdminLTE/dist/js/app.min.js"></script>
 </head>
