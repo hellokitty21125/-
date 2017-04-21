cc61cbb08ac96a0acc885e6d3f34832f310282e9
黄秀杰 <huangxiujie@126.com>
2017-04-21 15:25:26 +0800

    图片上传的表单验证

--- application/views/category/edit.php
+++ application/views/category/edit.php
@@ -93,6 +93,12 @@
   $(function () { 
     $('select').select2({
     });
+    $('form').submit(function (e) {
+      if ($('#avatar').val() == '') {
+        alert('请上传分类图');
+        e.preventDefault();
+      }
+    });
 
   });
 </script>
