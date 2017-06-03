ae017548d6d6815116ddd0e6cfd457175e9f7a3c
黄秀杰 <huangxiujie@163.com>
2017-06-03 22:55:46 +0800

    恢复支付URL

--- application/controllers/WXPay.php
+++ application/controllers/WXPay.php
@@ -6,46 +6,46 @@ use \LeanCloud\Object;
 class WXPay extends BaseController {
 	// pay order 
 	// 此函数即将废弃，已改用云函数实现，详见：http://blog.it577.net/index.php/archives/7/
-	// function index() {
-	// 	// 		初始化值对象
-	// 	$input = new WxPayUnifiedOrder();
-	// 	// 		文档提及的参数规范：商家名称-销售商品类目
-	// 	$input->SetBody($this->input->post('body'));
-	// 	// 		订单号应该是由小程序端传给服务端的，在用户下单时即生成，demo中取值是一个生成的时间戳
-	// 	$input->SetOut_trade_no($this->input->post('tradeNo'));
-	// 	// 		费用应该是由小程序端传给服务端的，在用户下单时告知服务端应付金额，demo中取值是1，即1分钱
-	// 	$input->SetTotal_fee($this->input->post('totalFee'));
-	// 	$input->SetNotify_url("https://lendoo.leanapp.cn/WXPay/notify");
-	// 	$input->SetTrade_type("JSAPI");
-	// 	// 		由小程序端传给服务端
-	// 	$input->SetOpenid($this->input->post('openid'));
-	// 	// 		向微信统一下单，并返回order，它是一个array数组
-	// 	$order = WxPayApi::unifiedOrder($input);
-	// 	// 		json化返回给小程序端
-	// 	header("Content-Type: application/json");
-	// 	echo $this->getJsApiParameters($order);
-	// }
+	function index() {
+		// 		初始化值对象
+		$input = new WxPayUnifiedOrder();
+		// 		文档提及的参数规范：商家名称-销售商品类目
+		$input->SetBody($this->input->post('body'));
+		// 		订单号应该是由小程序端传给服务端的，在用户下单时即生成，demo中取值是一个生成的时间戳
+		$input->SetOut_trade_no($this->input->post('tradeNo'));
+		// 		费用应该是由小程序端传给服务端的，在用户下单时告知服务端应付金额，demo中取值是1，即1分钱
+		$input->SetTotal_fee($this->input->post('totalFee'));
+		$input->SetNotify_url("https://lendoo.leanapp.cn/WXPay/notify");
+		$input->SetTrade_type("JSAPI");
+		// 		由小程序端传给服务端
+		$input->SetOpenid($this->input->post('openid'));
+		// 		向微信统一下单，并返回order，它是一个array数组
+		$order = WxPayApi::unifiedOrder($input);
+		// 		json化返回给小程序端
+		header("Content-Type: application/json");
+		echo $this->getJsApiParameters($order);
+	}
 
 	// 此函数即将废弃，已改用云函数实现，详见：http://blog.it577.net/
-	// private function getJsApiParameters($UnifiedOrderResult)
-	// {
-	// 	if(!array_key_exists("appid", $UnifiedOrderResult)
-	// 	|| !array_key_exists("prepay_id", $UnifiedOrderResult)
-	// 	|| $UnifiedOrderResult['prepay_id'] == "")
-	// 	{
-	// 		throw new WxPayException("参数错误");
-	// 	}
-	// 	$jsapi = new WxPayJsApiPay();
-	// 	$jsapi->SetAppid($UnifiedOrderResult["appid"]);
-	// 	$timeStamp = time();
-	// 	$jsapi->SetTimeStamp("$timeStamp");
-	// 	$jsapi->SetNonceStr(WxPayApi::getNonceStr());
-	// 	$jsapi->SetPackage("prepay_id=" . $UnifiedOrderResult['prepay_id']);
-	// 	$jsapi->SetSignType("MD5");
-	// 	$jsapi->SetPaySign($jsapi->MakeSign());
-	// 	$parameters = json_encode($jsapi->GetValues());
-	// 	return $parameters;
-	// }
+	private function getJsApiParameters($UnifiedOrderResult)
+	{
+		if(!array_key_exists("appid", $UnifiedOrderResult)
+		|| !array_key_exists("prepay_id", $UnifiedOrderResult)
+		|| $UnifiedOrderResult['prepay_id'] == "")
+		{
+			throw new WxPayException("参数错误");
+		}
+		$jsapi = new WxPayJsApiPay();
+		$jsapi->SetAppid($UnifiedOrderResult["appid"]);
+		$timeStamp = time();
+		$jsapi->SetTimeStamp("$timeStamp");
+		$jsapi->SetNonceStr(WxPayApi::getNonceStr());
+		$jsapi->SetPackage("prepay_id=" . $UnifiedOrderResult['prepay_id']);
+		$jsapi->SetSignType("MD5");
+		$jsapi->SetPaySign($jsapi->MakeSign());
+		$parameters = json_encode($jsapi->GetValues());
+		return $parameters;
+	}
 
 	public function notify() {
 		//获取通知的数据
@@ -70,12 +70,12 @@ class WXPay extends BaseController {
 	
 	// get openid & session_key
 	// 此函数即将废弃，已改用云函数实现，详见：http://blog.it577.net/index.php/archives/7/
-	// public function getSession() {
-	// 	$code = $this->input->post('code');
-	// 	$url = 'https://api.weixin.qq.com/sns/jscode2session?appid='.WxPayConfig::APPID.'&secret='.WxPayConfig::APPSECRET.'&js_code='.$code.'&grant_type=authorization_code';
- //            header("Content-Type: application/json");
- //            echo file_get_contents($url);
-	// }
+	public function getSession() {
+		$code = $this->input->post('code');
+		$url = 'https://api.weixin.qq.com/sns/jscode2session?appid='.WxPayConfig::APPID.'&secret='.WxPayConfig::APPSECRET.'&js_code='.$code.'&grant_type=authorization_code';
+            header("Content-Type: application/json");
+            echo file_get_contents($url);
+	}
 
 	// get access token
 	// private function getAccessToken() {
